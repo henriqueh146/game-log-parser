348e9d9580faf31095214068a19ca84f0ce2c2dd
Henrique <henriquecon1@gmail.com>
2022-01-18 16:20:08 -0300

    Correcting indentations

--- lib/classes/game_parser.rb
+++ lib/classes/game_parser.rb
@@ -1,10 +1,10 @@
 class GameParser
-    def initialize(file)
-        @file = file
-    end
+  def initialize(file)
+    @file = file
+  end
 
-    def print_first_line
-        raise Exception.new "File doesn't exist" unless File.file?(@file)
-        puts File.open(@file, &:readline)
-    end
+  def print_first_line
+    raise Exception.new "File doesn't exist" unless File.file?(@file)
+    puts File.open(@file, &:readline)
+  end
 end
