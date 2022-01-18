67ecc662bf45d1bd47b70885cfea00902d410808
Henrique <henriquecon1@gmail.com>
2022-01-18 09:54:45 -0300

    Correcting line_spec in order to test the classs GameParser, not the main (driver) file

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -1,9 +1,9 @@
-require 'main'
+require 'classes/game_parser'
 
 describe 'main' do
     it "Print the first line from games.log" do
         expect{
-            load('main.rb') 
+            GameParser.new("games.log").print_first_line 
         }.to output("  0:00 ------------------------------------------------------------\n").to_stdout
     end
 end
