8935dfdae2b91a4861ae205afae0342aa149199a
Henrique <henriquecon1@gmail.com>
2022-01-18 09:52:08 -0300

    Correcting the Gameparser class in order to access only the 1st line

--- lib/classes/game_parser.rb
+++ lib/classes/game_parser.rb
@@ -3,18 +3,8 @@ class GameParser
         @file = file
     end
 
-    def open_file
-        raise Exception.new "File doesn't exist" unless File.file?(@file)
-        file = File.open(@file)
-    end
-
-    def close_file(file)
-        file.close
-    end
-
     def print_first_line
-        file = open_file
-        puts file.read.split(/\n/)[0]
-        close_file(file)
+        raise Exception.new "File doesn't exist" unless File.file?(@file)
+        puts File.open(@file, &:readline)
     end
 end
--- spec/open_spec.rb
+++ spec/open_spec.rb
@@ -3,7 +3,7 @@ require 'classes/game_parser'
 describe 'GameParser open file exception' do
     it "Raise an exception when the file is absent" do
         expect{
-            GameParser.new("absent_file").open_file
+            GameParser.new("absent_file").print_first_line
     }.to raise_error
     end
 end
