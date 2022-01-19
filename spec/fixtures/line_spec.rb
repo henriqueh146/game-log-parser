5fd7fc948caf7adb83f6e4b388d5ef583071d8bd
Henrique <henriquecon1@gmail.com>
2022-01-19 12:11:48 -0300

    Change tests to one file only and rename it after the class file; change the games-fake.log locarion to fixtures directory

--- spec/fixtures/line_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'game_parser'
-
-describe '#print_first_line' do
-  it "Print the first line from games.log" do
-    expect{
-      puts GameParser.new("games-fake.log").get_first_line 
-    }.to output("  0:00 ------------------------------------------------------------\n").to_stdout
-  end
-end
--- spec/lib/open_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'game_parser'
-
-describe '#print_first_line' do
-  it "Raise an exception when the file is absent" do
-    expect{
-      GameParser.new("absent_file").get_first_line
-    }.to raise_error
-  end
-end
