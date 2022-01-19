cebcadae5b000e590a520f31d9dc043ba76bcf6b
Henrique <henriquecon1@gmail.com>
2022-01-19 10:47:44 -0300

    Testing the file existence inside the initialize

--- lib/game_parser.rb
+++ lib/game_parser.rb
@@ -1,10 +1,10 @@
 class GameParser
   def initialize(file)
+   raise Exception.new "File doesn't exist" unless File.file?(@file)
     @file = file
   end
 
   def print_first_line
-    raise Exception.new "File doesn't exist" unless File.file?(@file)
     puts File.open(@file, &:readline)
   end
 end
