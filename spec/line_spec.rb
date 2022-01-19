d0e65372c9d426529dc239dc7e8dcef2e8a6f880
Henrique <henriquecon1@gmail.com>
2022-01-18 22:41:14 -0300

    Use the 'fixture' games.log file named as games-fake.log in order to not open the complete original one

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -3,7 +3,7 @@ require 'classes/game_parser'
 describe '#print_first_line' do
   it "Print the first line from games.log" do
     expect{
-      GameParser.new("games.log").print_first_line 
+      GameParser.new("games-fake.log").print_first_line 
     }.to output("  0:00 ------------------------------------------------------------\n").to_stdout
   end
 end
