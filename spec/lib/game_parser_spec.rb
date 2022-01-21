a23fc7564d604e4af451844dc0817f86ee64928d
Henrique <henriquecon1@gmail.com>
2022-01-20 23:32:12 -0300

    Fixing the test structure of game_parser_spec.rb

--- spec/lib/game_parser_spec.rb
+++ spec/lib/game_parser_spec.rb
@@ -6,21 +6,25 @@ describe GameParser do
       GameParser.new("absent_file")
     }.to raise_error
   end
-end
+  
+  describe '#get_first_line' do
+    it "Print the first line from games.log" do
+      expect{
+        puts GameParser.new("spec/fixtures/games-fake.log").get_first_line 
+      }.to output("  0:00 ------------------------------------------------------------\n").to_stdout
+    end
+  end
 
-describe '#get_first_line' do
-  it "Print the first line from games.log" do
-    expect{
-      puts GameParser.new("spec/fixtures/games-fake.log").get_first_line 
-    }.to output("  0:00 ------------------------------------------------------------\n").to_stdout
+  describe '#get_log_metadata' do
+    it "Return a json object with the number of lines of a log file" do
+      file = GameParser.new("spec/fixtures/games-fake.log")
+      path, value = JSON.parse("{#{file.get_log_metadata}}").first
+      lines, number_of_lines = value.first
+      expect(File.exists?(path) && (lines == "lines")).to eql(true)
+    end
   end
 end
 
-describe '#get_log_metadata' do
-  it "Return a json object with the number of lines of a log file" do
-    file = GameParser.new("spec/fixtures/games-fake.log")
-    path, value = JSON.parse("{#{file.get_log_metadata}}").first
-    lines, number_of_lines = value.first
-    expect(File.exists?(path) && (lines == "lines")).to eql(true)
-  end
-end
+
+
+
