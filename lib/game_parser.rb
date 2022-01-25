a2b4ff1e92e7eb376b92e34724b687458bab0f0f
Henrique <henriquecon1@gmail.com>
2022-01-25 14:52:17 -0300

    Include player's name in metadata

--- lib/game_parser.rb
+++ lib/game_parser.rb
@@ -13,7 +13,8 @@ class GameParser
   def get_log_metadata
     path = get_file_path
     lines = get_number_of_lines
-    metadata = {path => {"lines" => lines}}
+    players = get_players
+    metadata = {path => {"lines" => lines, "players" => players}}
     JSON.pretty_generate(metadata)
   end
 
@@ -25,6 +26,23 @@ class GameParser
 
   def get_file_path
     File.expand_path(@file)
+  end
+
+  def get_players
+    file_lines = File.readlines(@file, chomp: true)
+    players = []
+    file_lines.each { |line|
+      if line.include?("Kill")
+        killer = line[/(?<=: )([a-zA-Z ]*)(?= killed )/m, 1]
+        killed = line[/(?<= killed )([a-zA-Z ]*)(?= by)/m, 1]
+        players.push(killer) unless players.include?(killer)
+        players.push(killed) unless players.include?(killed)
+      elsif line.include?("ClientUserinfoChanged")
+        player = line[/ n\\([a-zA-Z ]*)\\t/m, 1]
+        players.push(player) unless players.include?(player)
+      end
+    }
+    players.compact
   end
 
 end
