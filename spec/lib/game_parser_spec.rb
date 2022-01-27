33bc290d614db042e5ec63abadd04035e01548ad
Henrique <henriquecon1@gmail.com>
2022-01-27 08:49:41 -0300

    Update fixtures files in order to the test cover all the cases

--- spec/lib/game_parser_spec.rb
+++ spec/lib/game_parser_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: false
+
 require 'game_parser'
 
 describe GameParser do
@@ -16,8 +18,9 @@ describe GameParser do
     it 'Return a json object with the number of lines of a log file' do
       parser = GameParser.new('spec/fixtures/games-fake.log')
       expect(parser.log_metadata).to eql("{\n  \"/home/henrique/projects/job-code/game-log-parser/spec/fixtures/"\
-        "games-fake.log\": {\n    \"lines\": 10,\n    \"players\": [\n      \"Isgalamido\"\n    ],\n    \"kills\": {\n"\
-        "      \"Isgalamido\": 0\n    },\n    \"total_kills\": 0\n  }\n}")
+        "games-fake.log\": {\n    \"lines\": 22,\n    \"players\": [\n      \"Isgalamido\",\n      \"Dono da Bola\",\n"\
+        "      \"Mocinha\"\n    ],\n    \"kills\": {\n      \"Isgalamido\": 2,\n      \"Dono da Bola\": 0,\n      "\
+        "\"Mocinha\": 0\n    },\n    \"total_kills\": 2\n  }\n}")
     end
   end
 end
