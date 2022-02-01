afbc3e518a102458e5f35785c72a3d0f104d6d8a
Henrique <henriquecon1@gmail.com>
2022-02-01 13:46:30 -0300

    Updating test file in order to get a hash instead of a Json string

--- spec/lib/game_parser_spec.rb
+++ spec/lib/game_parser_spec.rb
@@ -17,10 +17,9 @@ describe GameParser do
   describe '#log_metadata' do
     it 'Return a json object with the number of lines of a log file' do
       parser = GameParser.new('spec/fixtures/games-fake.log')
-      expect(parser.log_metadata).to eql("{\n  \"/home/henrique/projects/job-code/game-log-parser/spec/fixtures/"\
-        "games-fake.log\": {\n    \"lines\": 22,\n    \"players\": [\n      \"Isgalamido\",\n      \"Dono da Bola\",\n"\
-        "      \"Mocinha\"\n    ],\n    \"kills\": {\n      \"Isgalamido\": 2,\n      \"Dono da Bola\": 0,\n      "\
-        "\"Mocinha\": 0\n    },\n    \"total_kills\": 2\n  }\n}")
+      expect(parser.log_metadata).to eql({ '/home/henrique/projects/job-code/game-log-parser/spec/fixtures/games-fake'\
+        '.log' => { 'lines' => 31, 'players' => ['Isgalamido', 'Dono da Bola', 'Mocinha'],
+                    'kills' => { 'Isgalamido' => 3, 'Dono da Bola' => 0, 'Mocinha' => 0 }, 'total_kills' => 3 } })
     end
   end
 end
